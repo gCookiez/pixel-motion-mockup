60a9173e8487f4facc89be847c0e44ab2014addc
Marcus Gajo <musou_saber@proton.me>
2026-03-19 12:21:50 +0800

    Adjusted content
    - rearranged content to accommodate with scss and bootstrap

---
+++ src/components/header/nav-bar.js
@@ -0,0 +1,44 @@
+import * as bootstrap from 'bootstrap';
+
+
+
+export function columnBase(classes) {
+    const columnBase = document.createElement('div');
+    columnBase.classList.add('col', ...classes);
+    return columnBase;
+}
+
+
+// TODO: Find Icon pack for webpage
+export function hamburgerButton(content) {
+    const columnContent = columnBase(['w-25', 'h-auto', 'd-inline-block']);
+    const title = document.createElement('h4');
+    title.innerHTML = `${content || ''}`
+    columnContent.append(title);
+
+    return columnContent;
+}
+
+export function navDivSimple() {
+    const navBarDiv = document.createElement('div');
+    const rowContentsDiv = document.createElement('div');
+   
+    const hamburger = hamburgerButton('hm');
+    const logo = hamburgerButton('logo');
+    const details = hamburgerButton('');
+    
+
+    const navBarClasses = ['container-lg', 'text-center', 'bg-light']
+    const buttonClasses = ['row', 'align-items-start', 'bg-light', 'text-dark']
+
+    navBarDiv.classList.add(...navBarClasses);
+    rowContentsDiv.classList.add(...buttonClasses);
+
+    rowContentsDiv.append(hamburger, logo, details);
+
+    navBarDiv.append(rowContentsDiv);
+
+    return navBarDiv;
+}
+
+
---
+++ src/js/main.js
@@ -0,0 +1,19 @@
+
+import '../scss/index.scss';
+import { navDivSimple } from '../components/header/nav-bar';
+
+// const app = document.querySelector('#app');
+
+// const divTest = document.createElement('div');
+// const hOneTest = document.createElement('h1');
+// const buttonTest = document.createElement('button');
+
+// divTest.classList.add('container', 'py-4', 'px-3', 'mx-auto')
+// hOneTest.innerHTML = 'Hello, Bootstrap and Vite';
+// buttonTest.classList.add('btn','btn-primary')
+// buttonTest.innerHTML = 'Primary Button';
+
+
+// divTest.append(hOneTest,buttonTest);
+
+app.append(navDivSimple());
