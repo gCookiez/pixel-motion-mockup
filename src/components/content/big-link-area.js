fd8bb1410b39b13da801684cdec00a880f06004d
gCookiez <sucramgajo@gmail.com>
2026-03-23 17:40:35 +0800

    Added linear-Gradient to big links
    updated css to add linear-gradient pattern to big links

--- src/components/content/big-link-area.js
+++ src/components/content/big-link-area.js
@@ -1,18 +1,20 @@
-function bigLinkTemplate(title) {
+function bigLinkTemplate(title, className) {
     const bigLinkItem = document.createElement('div');
     const bigLinkTitle = document.createElement('h2');
-    bigLinkItem.classList.add('big-link-item');
+    bigLinkItem.classList.add('big-link-item', className);
     bigLinkTitle.innerHTML = `${title ? title : 'title'}`;
+    bigLinkTitle.classList.add('big-link-title');
     bigLinkItem.append(bigLinkTitle);
     return bigLinkItem;
 }
 
-const bigLinkAreaContainer = document.createElement('div');
+const bigLinkAreaContainer = document.createElement('div') 
 const bigLinkTitles = ['TEST RIDE', 'PARTS & ACCESSORIES'];
 bigLinkAreaContainer.classList.add('big-link-container');
 
 for  (var title of bigLinkTitles) {
-    const bigLinkItem = bigLinkTemplate(title);
+    const className = title.replace("&", "").split(" ").join('_').toLowerCase();
+    const bigLinkItem = bigLinkTemplate(title, className);
     bigLinkAreaContainer.append(bigLinkItem);
 }
 
