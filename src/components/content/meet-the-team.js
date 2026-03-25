bae6a0a5d876af60222472105a558e815b3f1175
gCookiez <sucramgajo@gmail.com>
2026-03-25 20:04:06 +0800

    Added styling for Search and meet the team
    -added page title on search and modified search bar
    
    -added initial styling for meet the teams

--- src/components/content/meet-the-team.js
+++ src/components/content/meet-the-team.js
@@ -1,3 +1,7 @@
+import { carouselFiller } from '../recycle/carousel';
+import { containerTemplate } from '../recycle/class-list';
+import { bootstrapRows, bootstrapColumns} from '../recycle/rows-and-columns'
+import {shopButtons} from '../recycle/non-standard-buttons';
 
 function teamMemberHeads() {
     const staffHead = document.createElement('div');
@@ -7,27 +11,55 @@ function teamMemberHeads() {
 }
 
 const teamContainer = document.createElement('div');
+const teamtitleMeetCluster = bootstrapRows('team-title-meet-cluster p-3');
 const teamTitle = document.createElement('h1');
-const teamMeet = document.createElement('div');
+const teamMeet = shopButtons('meet-team-btn', 'MEET OUR STAFF');
 const teamCarousel = document.createElement('div');
+const {list, carouselModule} = new carouselFiller();
+
+const cloneDivHead = document.createElement('div')
 
 const teamSize = 3;
 
 
 //add additional container for padding
 teamContainer.classList.add('team-container');
-teamMeet.classList.add('meet-team-btn');
 teamCarousel.classList.add('team-carousel');
+teamTitle.classList.add('team-title', 'pt-3','pb-1');
+teamMeet.classList.add('meet-team-btn');
 
 teamTitle.innerHTML = `Meet Our Team`;
-teamMeet.innerHTML = 'MEET OUR STAFF';
 
+teamtitleMeetCluster.append(teamTitle,teamMeet)
+
+
+// var tempClone = cloneDivHead.cloneNode(true);
+var tempClone = null;
+var counter = 0;
 
 for (var i = 0; i < teamSize; i++) {
-    teamCarousel.append(teamMemberHeads());
+    if (tempClone != null && i % 2 == 0) {
+        carouselModule.addSlide(tempClone, counter);
+        counter++;
+        tempClone = null;
+    }
+
+    if (tempClone == null) {
+      tempClone = cloneDivHead.cloneNode(true);
+      tempClone.classList.add('flex-team');  
+    } 
+    tempClone.append(teamMemberHeads());
+
+    if (i === teamSize - 1) {
+        carouselModule.addSlide(tempClone, counter);
+        counter++;
+        tempClone = null;
+    }
 }
 
-teamContainer.append(teamTitle, teamMeet, teamCarousel)
+
+teamCarousel.append(list);
+teamContainer.append(teamtitleMeetCluster, teamCarousel)
 
 export { teamContainer };
 
--- src/components/content/search-products.js
+++ src/components/content/search-products.js
@@ -1,6 +1,7 @@
 import { containerTemplate } from '../recycle/class-list';
 import { shopButtons } from '../recycle/non-standard-buttons';
 import { trimmedModules } from '../recycle/trimmed-filenames';
+import { bootstrapRows, bootstrapColumns } from '../recycle/rows-and-columns'
 
 const imagesContext = import.meta.glob('../../../public/hero.jpg', { eager: true, import: 'default' });
 
@@ -8,25 +9,34 @@ const trimmedModule = trimmedModules(imagesContext)
 
 
 const firstContainer = document.createElement('div');
+const title = document.createElement('h2');
+const titleContainer = bootstrapRows('title-container d-flex align-items-center p-3 pb-4');
 const buttonContainer = document.createElement('div');
-const searchContainer = document.createElement('div');
+const searchContainer = bootstrapRows('search-area ms-0')
+const searchPlaceholder = document.createElement('span');
+const searchlhs =  bootstrapColumns('search-lhs col-2')
+const searchrhs =  bootstrapColumns('search-rhs d-flex col-10 align-items-center')
 
 const buttonNames = ["SHOP NEW", "SHOP-PREOWNED", "SPECIALS"];
 
-
-firstContainer.classList.add('search-container', ...containerTemplate('list'));
+title.classList.add('main-page-title', 'text-center');
+title.innerHTML = 'Welcome to <br> Triumph of Anytown';
+firstContainer.classList.add('search-container', 'p-4', 'pt-2', 'pb-5');
 firstContainer.setAttribute('style', `background: url(${trimmedModule['HERO']})`)
 buttonContainer.classList.add('button-grid');
-searchContainer.classList.add('search-area');
+searchPlaceholder.classList.add('search-placeholder')
+searchPlaceholder.innerHTML = 'SEARCH RIDES'
 
 
 for (var buttonText of buttonNames) {
     const shopButton = shopButtons('def filter-buttons', buttonText); 
     buttonContainer.append(shopButton);
 }
-
+searchrhs.append(searchPlaceholder);
+searchContainer.append(searchlhs, searchrhs);
 buttonContainer.append(searchContainer);
-firstContainer.append(buttonContainer);
+titleContainer.append(title)
+firstContainer.append(titleContainer, buttonContainer);
 
 
 
