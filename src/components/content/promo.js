95c349680c0d4e9e722d084fb8ff4253e41be62c
Marcus Gajo <musou_saber@proton.me>
2026-04-02 01:39:05 +0800

    Added changes in promo section

--- src/components/content/promo.js
+++ src/components/content/promo.js
@@ -1,18 +1,24 @@
 import { trimmedModules } from "../recycle/trimmed-filenames";
 import { containerTemplate } from "../recycle/class-list";
 import { underLineButton } from "../recycle/non-standard-buttons";
-const imagesContext = import.meta.glob('@public/promo.jpg', { eager: true, import: 'default' });
-const trimmedModule = trimmedModules(imagesContext);
+import { bootstrapColumns, bootstrapRows } from "../recycle/rows-and-columns";
+// const imagesContext = import.meta.glob('@public/promo.jpg', { eager: true, import: 'default' });
+// const trimmedModule = trimmedModules(imagesContext);
 
 const promoContainer = document.createElement('div');
 const promoOverlay = document.createElement('div');
+const promoObject = bootstrapRows('promo-content');
+const promoObjectCol = bootstrapColumns('promo-content-lhs col-12 col-sm-7');
+
 const promoTitle = document.createElement('h1');
-const promoLinks = document.createElement('div')
+const promoLinks = document.createElement('div');
+const imageExt = document.createElement('div');
+
 const promoLinkTitles = ['PROMOTIONS', 'EVENTS'];
 
-promoContainer.classList.add('promo-container', 'd-flex', 'flex-column', 'mt-3');
-promoOverlay.classList.add('promo-overlay', ...containerTemplate('list'))
-promoContainer.setAttribute('style', `background-image: url(${trimmedModule['PROMO']})`)
+promoContainer.classList.add('promo-container', 'd-flex', 'flex-row', 'mt-3', 'position-relative');
+promoOverlay.classList.add('promo-overlay', ...containerTemplate('list'), 'w-100', 'h-50')
+// promoContainer.setAttribute('style', `background-image: url(${trimmedModule['PROMO']})`)
 
 promoTitle.innerHTML = `VIEW CURRENT PROMOTIONS AND EVENTS`;
 promoTitle.classList.add('promo-title');
@@ -23,7 +29,11 @@ for (var promoLink of promoLinkTitles) {
 	promoLinks.append(iterPromoLink);
 }
 
-promoOverlay.append(promoTitle, promoLinks)
-promoContainer.append(promoOverlay);
+imageExt.classList.add('mini-img-promo', 'position-absolute')
+
+promoObjectCol.append(promoTitle,promoLinks)
+promoObject.append(promoObjectCol)
+promoOverlay.append(promoObject)
+promoContainer.append(promoOverlay, imageExt);
 
 export { promoContainer };
